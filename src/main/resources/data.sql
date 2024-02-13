9d6a5c64180de2a80b7171c81601aaa4de65dce4
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-02-13 15:24:06 +0900

    data sql 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into board (id, title, contents) values (1, '테스트 제목1', '테스트 내용1');
+insert into board (id, title, contents) values (2, '테스트 제목2', '테스트 내용2');
+insert into board (id, title, contents) values (3, '테스트 제목3', '테스트 내용3');
