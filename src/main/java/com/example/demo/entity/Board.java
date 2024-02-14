da70d0bcc6829af2501d0e1d2a565f245c8731d5
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-02-14 10:32:33 +0900

    No default constructor for entity 오류 해결

--- src/main/java/com/example/demo/entity/Board.java
+++ src/main/java/com/example/demo/entity/Board.java
@@ -1,6 +1,7 @@
 package com.example.demo.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Board {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
