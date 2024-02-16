68f5e291fa7151d96606469bf6bd1226c2c64dd2
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-02-16 16:17:16 +0900

    user 테스트 코드 작성

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,25 @@
 package com.example.demo;
 
 import com.example.demo.entity.Board;
+import com.example.demo.entity.User;
 import com.example.demo.repository.BoardRepository;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.BoardService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 	@Autowired
 	private BoardRepository boardRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+	BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
 	/**
 	 *  Board 테스트 데이터 300개 생성
@@ -23,6 +32,34 @@ class DemoApplicationTests {
 			Board board = new Board(title, content);
 			this.boardRepository.save(board);
 		}
+	}
+
+	/**
+	 *  admin user 생성
+	 */
+	@Test
+	void createTestUser(){
+		String encPw = passwordEncoder.encode("admin");
+		User testUser = User.builder()
+				.userId("admin")
+				.userPw(encPw)
+				.userName("admin")
+				.build();
+
+		userRepository.save(testUser);
+	}
+
+	/**
+	 * 유저 정보 검색 후 비밀번호 비교
+	 */
+	@Test
+	void test_2(){
+		String encPassword = passwordEncoder.encode("admin");
+
+		User user = userRepository.findByUserId("admin")
+				.orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+		assertThat(user.getUserPw()).isEqualTo(encPassword);
 	}
 
 }
