6b6d369982b8c82950b6b9ad307c1978799cd5c8
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-02-19 09:26:33 +0900

    TokenRequestFilter 생성 및 적용
    받은 요청마다 토큰을 꺼내 확인하는 필터 클래스 적용

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import com.example.demo.entity.Member;
 import com.example.demo.service.MemberService;
+import com.example.demo.util.TokenRequestFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,12 +14,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final MemberService memberService;
+    private final TokenRequestFilter tokenRequestFilter;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -33,17 +36,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests() // 토큰을 활용하는 경우 모든 요청에 대해 접근이 가능하도록 함
                 .anyRequest().permitAll()
                 .and() // 토큰을 활용하면 세션이 필요 없으므로 STATELESS로 설정하여 Session을 사용하지 않는다.
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and() // form 기반의 로그인에 대해 비활성화 한다.
                 .formLogin()
-                .disable();
-        //.addFilterBefore(tokenRequestFilter, UsernamePasswordAuthenticationFilter.class)
-
+                .disable()
+                .addFilterBefore(tokenRequestFilter, UsernamePasswordAuthenticationFilter.class);
         http.cors();
     }
 }
