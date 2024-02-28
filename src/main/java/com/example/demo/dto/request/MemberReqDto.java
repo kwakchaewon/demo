0c93506ecc4aee6071ad3752434ff9e6d7466a07
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-02-28 14:54:19 +0900

    불필요한 코드 제거

--- src/main/java/com/example/demo/dto/request/MemberReqDto.java
+++ src/main/java/com/example/demo/dto/request/MemberReqDto.java
@@ -11,24 +11,15 @@ import javax.validation.constraints.*;
 @Getter
 @NoArgsConstructor
 public class MemberReqDto {
-    // @NotNull: Null 만 허용 x
-    // @NotEmpty: Null, "" 허용 x
-    // @NotBlank, Null, "", " " 허용 x
-
-//    @Pattern(regexp = "^[a-zA-Z0-9]{4,12}$", message = "아이디가 올바르지 않습니다.</br>4~12자의 숫자,영문으로 생성 가능합니다.")
     @Pattern(regexp = "^[a-zA-Z0-9]{4,12}$", message = "아이디: 4~12자의 숫자,영문을 입력해주세요.")
     private String userId;
 
-//    @NotEmpty
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
             message = "비밀번호: 8자 이상의 영문, 숫자, 특수문자를 입력해주세요")
-//            message = "비밀번호가 올바르지 않습니다.</br>8자 이상의 영문, 숫자, 특수문자로 생성 가능합니다.")
     private String userPw;
 
-//    @NotEmpty
     private String userPwCk;
 
-//    @NotEmpty
     @Pattern(regexp = "^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\\.[A-za-z0-9\\-]+",
             message = "이메일: 이메일 주소 양식을 입력해주세요.")
     private String email;
@@ -36,17 +27,5 @@ public class MemberReqDto {
     @AssertTrue(message = "비밀번호와 비밀번호 확인이 일치하지 않습니다.")
     public boolean isPasswordConfirmed() {
         return userPw != null && userPw.equals(userPwCk);
-    }
-
-
-    public MemberReqDto(String userId, String email, String userPw) {
-        this.userId = userId;
-        this.email = email;
-        this.userPw = userPw;
-//        this.userPwCk = userPwCk;
-    }
-
-    public void setEncodedPw(String encodedPw){
-        this.userPw = encodedPw;
     }
 }
--- src/main/java/com/example/demo/dto/response/BoardDto.java
+++ src/main/java/com/example/demo/dto/response/BoardDto.java
@@ -17,12 +17,11 @@ import java.time.LocalDateTime;
 @Builder
 public class BoardDto {
     private Long id;
-//    @NotBlank(message = "제목은 필수 항목입니다.")
+
     @NotEmpty(message = "제목은 필수 항목입니다.")
     @Size(max = 200, message = "제목은 200자 이상 넘길 수 없습니다.")
     private String title;
 
-//    @NotBlank(message="내용은 필수항목입니다.")
     @NotEmpty(message = "제목은 필수 항목입니다.")
     private String contents;
     private String createdAt;
