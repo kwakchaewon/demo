31927d7da9fa584d0fe28a97cb426986a69e8848
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-02-28 18:03:56 +0900

    불필요한 코드 정리

--- src/main/java/com/example/demo/dto/response/BoardDto.java
+++ src/main/java/com/example/demo/dto/response/BoardDto.java
@@ -6,10 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-import java.time.LocalDateTime;
+
 
 @Data
 @AllArgsConstructor
