22227d4fd79086f384d8472b9d9006060e213c89
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-03-02 09:53:22 +0900

    Update CommentController.java

--- src/main/java/com/example/demo/controller/CommentController.java
+++ src/main/java/com/example/demo/controller/CommentController.java
@@ -9,6 +9,8 @@ import com.example.demo.entity.Member;
 import com.example.demo.service.BoardService;
 import com.example.demo.service.CommentService;
 import com.example.demo.service.MemberService;
+import com.example.demo.util.exception.Constants;
+import com.example.demo.util.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -35,14 +37,14 @@ public class CommentController {
     public ResponseEntity createCommnet(
             @PathVariable Long id,
             @RequestBody CommentReqDto commentReqDto,
-            @RequestHeader("ACCESS_TOKEN") String authorizationHeader){
+            @RequestHeader("ACCESS_TOKEN") String authorizationHeader) throws CustomException {
 
         String _userId = boardService.getUserIdByToken(authorizationHeader, secret_access);
         Member _member = this.memberService.getMemberByUserId(_userId);
 
         // 빈 내용 유효성 검사
         if (commentReqDto.getContents().trim().isEmpty()) {
-            return new ResponseEntity<>("빈 내용 입력할 수 없습니다.", HttpStatus.BAD_REQUEST);
+            throw  new CustomException(HttpStatus.BAD_REQUEST, Constants.ExceptionClass.BOARD_ONLY_BLANk);
         } else {
             commentService.commentSave(_member.getUserId(), id, commentReqDto);
             return new ResponseEntity<>(HttpStatus.OK);
