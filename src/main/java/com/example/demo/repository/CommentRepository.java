9e23561c515c948948a8e15dfc259a84e229390f
kwak chae won <74225015+kwakchaewon@users.noreply.github.com>
2024-04-02 14:45:59 +0900

    댓글 오름차순 정렬

--- src/main/java/com/example/demo/repository/CommentRepository.java
+++ src/main/java/com/example/demo/repository/CommentRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<CommentDto> findCommentDtoByBoard(Board board);
-    List<CommentDto> findCommentDtoByBoardIdOrderByCreatedAtDesc(Long id);
+    List<CommentDto> findCommentDtoByBoardIdOrderByCreatedAtAsc(Long id);
 }
